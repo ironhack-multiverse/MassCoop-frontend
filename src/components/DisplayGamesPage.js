eca82debdf48f41cb1f54137a260824d7441c6f2
Ola Bonati <olabonati@gmail.com>
2023-07-04 19:55:45 +0200

    displaygames page working

--- src/components/DisplayGamesPage.js
+++ src/components/DisplayGamesPage.js
@@ -3,15 +3,19 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import AddGame from "./AddGame";
+import GameCard from "./GameCard";
 
     function DisplayGamesPage() {
-        const [games, setGames] = useState([]);
+        const [games, setGames] = useState(null);
       
         const getAllGames = () => {
           axios
             .get(`${process.env.REACT_APP_SERVER_URL}/api/games`)
-            .then((response) => setGames(response.data))
-            .catch((error) => console.log(error));
+            .then((response) => 
+           setGames(response.data))
+           .catch((error) => console.log(error));
+
         };
       
         // We set this effect will run only once, after the initial render
@@ -19,29 +23,25 @@ import { Link } from "react-router-dom";
         useEffect(() => {
           getAllGames();
         }, [] );
-      
+
+
         return (
-            <div className="GameListPage">
-              
-              <AddGame refreshProjects={getAllGames} />
-              
-              { projects.map((project) => <GameCard key={project._id} {...project} />  )} 
-               
-            </div>
-          );
-        }
-        
+            <section className="GamesList">
+              <h1>Games List</h1>
+          
+              {games && games.map((element) => (
+                <div key={element._id}>
+                  <GameCard  {...element.game}  { ...element}/>
+                  <h3> Name: {element.game?.name}</h3>
+                   <img src={element.game?.cover?.url} alt={element.name} /> 
+       {/*    
+                  
+                  <Link to={`/games/${element._id}`}>More details</Link> */}
+                </div>
+              ))} 
+            </section>
+          );}
+          
 
 
-    return <h1>THIS IS THE GAME PAGE</h1>
-
-
-
-
-
-
-
-
-}
-
-export default Games
+export default DisplayGamesPage;
