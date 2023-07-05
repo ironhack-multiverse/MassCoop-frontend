d676ab58165f109b9d8f2568b7d99e9430fb1cc5
Ola Bonati <olabonati@gmail.com>
2023-07-05 10:18:37 +0200

    home page with conditional rendering

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,13 +1,26 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
 
-function GameCard ( { name, summary, _id } ) {
-  
+function GameCard({ name, summary, cover, campaigncoop, offlinecoop, onlinecoop, onlinemax }) {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleDetailsClick = () => {
+    setShowDetails(!showDetails);
+  };
+
   return (
-    <div className="GameCard card">
-      <Link to={`/games/${_id}`}>
-        <h3>{name}</h3>
-      </Link>
-      <p style={{ maxWidth: "400px" }}>{summary} </p>
+    <div className="GameCard">
+      <h3>{name}</h3>
+      {cover && <img src={cover.url} alt={name} />}
+      <button onClick={handleDetailsClick}>{showDetails ? "Hide Details" : "Show Details"}</button>
+      {showDetails && (
+        <div>
+          <p>Summary: {summary}</p>
+          <p>Campaign Co-op: {campaigncoop ? "Yes" : "No"}</p>
+          <p>Offline Co-op: {offlinecoop ? "Yes" : "No"}</p>
+          <p>Online Co-op: {onlinecoop ? "Yes" : "No"}</p>
+          <p>Online Max Players: {onlinemax}</p>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,17 +1,63 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import NavBar from "../components/NavBar";
+import GameCard from "../components/GameCard";
+const API_URL = "http://localhost:5005";
 
 function HomePage() {
-    return (
-      <div>
-        <h1>Home Page</h1>
-<button>Local games</button>
-<button>Online games</button>
+  const [games, setGames] = useState([]);
+  const [filteredGames, setFilteredGames] = useState([]);
+  const [filterValue, setFilterValue] = useState("");
 
+  useEffect(() => {
+    refreshGames();
+  }, []);
 
+  const refreshGames = () => {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL || API_URL}/api/games`)
+      .then((response) => {
+        setGames(response.data);
+        setFilteredGames(response.data);
+      })
+      .catch((error) => console.log(error));
+  };
 
+  const filterGames = (filterType) => {
+    let filtered = [];
+    if (filterType === "local") {
+      filtered = games.filter((game) => game.offlinecoop === true);
+    } else if (filterType === "online") {
+      filtered = games.filter((game) => game.onlinecoop === true);
+    } else {
+      filtered = games;
+    }
+    setFilteredGames(filtered);
+  };
 
-      </div>
-    );
-  }
+  const handleFilterClick = (filterType) => {
+    filterGames(filterType);
+  };
+
+  return (
+    <div>
+      <h1>Home Page</h1>
   
-  export default HomePage;
+      <button onClick={() => handleFilterClick("local")}>Local games</button>
+      <button onClick={() => handleFilterClick("online")}>Online games</button>
+  
+      <ul>
+        {filteredGames.map((element) => (
+          <div key={element._id}>
+            <GameCard {...element.game} {...element} />
+            <h3>Name: {element.game?.name}</h3>
+            <img src={element.game?.cover?.url} alt={element.game?.name} />
+            <button>see details</button>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+  
+        }
+export default HomePage;
