a732638e18e4a287eae715348b9380ce2e6f19db
Ola Bonati <olabonati@gmail.com>
2023-07-06 14:42:00 +0200

    fixed path for isLogged in current user on the delete button

--- src/pages/GameDetailsPage.js
+++ src/pages/GameDetailsPage.js
@@ -1,26 +1,42 @@
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import AddReview from "../components/AddReview";
-//import gamesService from "../services/games.services";
 import axios from "axios";
 import ReviewCard from "../components/ReviewCard";
+import { AuthContext } from "../context/auth.context";
+import { useContext } from "react";
 const API_URL = "http://localhost:5005";
 
 function GamesDetailsPage(props) {
+  const storedToken = localStorage.getItem("authToken");
+
+  const { user} = useContext(AuthContext);
+
   const [game, setGame] = useState(null);
   const { gameId } = useParams();
+  const navigate = useNavigate();
 
   const getGame = () => {
     axios
-      .get(
-        `${process.env.REACT_APP_SERVER_URL || API_URL}/api/games/${gameId}`
-        // { headers: { Authorization: `Bearer ${storedToken}` } }
-      )
-
-      // gamesService.getGame(gameId)
+      .get(`${process.env.REACT_APP_SERVER_URL || API_URL}/api/games/${gameId}`)
       .then((response) => {
         const oneGame = response.data;
         setGame(oneGame);
+      })
+      .catch((error) => console.log(error));
+  };
+//DELETE GAME
+  const handleDeleteGame = () => {
+    console.log('ID game', gameId)
+    axios
+      .delete(`${process.env.REACT_APP_SERVER_URL}/api/games/${gameId}` 
+      ,{ headers: { Authorization: `Bearer ${storedToken}` } }
+      )
+      .then((response) => {
+
+        console.log(response.data);
+        // Navigate to the games list page after successful deletion
+        navigate("/games");
       })
       .catch((error) => console.log(error));
   };
@@ -28,34 +44,22 @@ function GamesDetailsPage(props) {
   useEffect(() => {
     getGame();
   }, []);
-  console.log(game);
 
   return (
     <div className="GameDetails">
-      {" "}
       <br />
       {game && (
         <>
           {game.game && game.game.cover && game.game.cover.url && (
             <img src={game.game.cover.url} alt="cover" />
           )}
-
           <h1>Game name: {game.game.name}</h1>
-          <p>Summary :{game.game.summary}</p>
-          <p>
-            Playable coop campaign :{" "}
-            {game.campaigncoop ? "available" : "unavailable"}
-          </p>
-          <p>
-            Local coop multiplayer mode :{" "}
-            {game.offlinecoop ? "available" : "unavailable"}
-          </p>
-          <p>
-            Online multiplayer mode :{" "}
-            {game.onlinecoop ? "available" : "unavailable"}
-          </p>
-          <p>Maximum online players : {game.onlinemax || 0}</p>
-          <p>Maximum offline players : {game.offlinemax || 0}</p>
+          <p>Summary: {game.game.summary}</p>
+          <p>Playable coop campaign: {game.campaigncoop ? "available" : "unavailable"}</p>
+          <p>Local coop multiplayer mode: {game.offlinecoop ? "available" : "unavailable"}</p>
+          <p>Online multiplayer mode: {game.onlinecoop ? "available" : "unavailable"}</p>
+          <p>Maximum online players: {game.onlinemax || 0}</p>
+          <p>Maximum offline players: {game.offlinemax || 0}</p>
           <p>{game.review}</p>
           <p>{game.rating}</p>
         </>
@@ -65,9 +69,11 @@ function GamesDetailsPage(props) {
       <Link to="/games">
         <button>Back to games</button>
       </Link>
+      {(user && user._id === game?.owner) && <p>Hello {user.name}</p>}
       <Link to={`/games/edit/${gameId}`}>
         <button>Edit Game</button>
       </Link>
+      <button onClick={handleDeleteGame}>Delete game</button>
     </div>
   );
 }
